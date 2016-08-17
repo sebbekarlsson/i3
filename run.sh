7dce98893b9e003c45af4e63ce2e055bdeb679fc
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-08-17 22:32:55 +0200

    added install script

---
+++ run.sh
@@ -0,0 +1,29 @@
+cd ~/
+
+# Update
+sudo pacman -Syuu
+
+# Install base packages
+yes | sudo pacman -S xorg-xinit xorg-xrandr
+yes | sudo pacman -S gvim chromium gnome-control-center xfce4-terminal
+yes | sudo pacman -S git
+
+# Install vundle for vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install yaourt
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+
+# Install i3-gaps
+yes | yaourt i3-gaps
+yes | sudo pacman -S i3status feh
+
+# Set xfce4-terminal config
+cp ./terminalrc ~/.config/xfce4/terminal/terminalrc
