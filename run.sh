089f666a3109ef5f4f452ea0a4e9e17debbbfdd3
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2017-04-09 15:12:56 +0200

    now using termite instead of xfce4-terminal

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo pacman -Syuu
 
 # Install base packages
 yes | sudo pacman -S xorg-xinit xorg-xrandr
-yes | sudo pacman -S gvim chromium gnome-control-center xfce4-terminal dmenu
+yes | sudo pacman -S gvim chromium gnome-control-center termite dmenu
 yes | sudo pacman -S git
 
 # Install vundle for vim
