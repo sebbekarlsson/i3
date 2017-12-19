f0045052336fcc2949dd31953f2d08e3a501749c
ianertson <ianertson@gmail.com>
2017-12-19 14:44:53 +0100

    updated install script and added setup for bumblebee

--- run.sh
+++ run.sh
@@ -6,8 +6,16 @@ sudo pacman -Syuu
 # Install base packages
 yes | sudo pacman -S xorg-xinit xorg-xrandr
 yes | sudo pacman -S gvim chromium gnome-control-center termite dmenu
+yes | sudo pacman -S python-pip python2-pip
 yes | sudo pacman -S rofi
 yes | sudo pacman -S git
+yes | sudo pacman -S xbindkeys
+yes | sudo pacman -S alsa-utils
+yes | sudo pacman -S ttf-font-awesome
+yes | sudo pacman -S i3lock imagemagick
+
+# python linter
+sudo pip install flake8
 
 # Install vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -25,6 +33,7 @@ cd ..
 # Install i3-gaps
 yes | yaourt i3-gaps
 yes | sudo pacman -S i3status feh
+yes | yaourt ttf-material-icons
 
 # Set xfce4-terminal config
 cp ./terminalrc ~/.config/xfce4/terminal/terminalrc
